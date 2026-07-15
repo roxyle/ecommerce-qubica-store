c0f0c807344c6b6a6d250b4473564dad4299066f
roxyle <ylenia.rossi@outlook.com>
2026-07-15 20:38:13 +0200

    feat/api: aggiunte funzioni per le get categoria oltre che per prodotto

--- src/api/fakeStoreApi.ts
+++ src/api/fakeStoreApi.ts
@@ -2,6 +2,24 @@ import type { Product } from '../types/product';
 
 const BASE_URL = "https://fakestoreapi.com";
 
+export async function getAllCategories(): Promise<string[]> {
+    const response= await fetch(`${BASE_URL}/products/categories`)
+
+    if (!response.ok) throw new Error("Errore nel recupero delle categorie");
+
+    
+    return response.json()
+}
+
+export async function getProductsByCategory(category:string): Promise<Product[]>{
+    const response= await fetch(`${BASE_URL}/products/category/${category}`)
+
+    if (!response.ok) throw new Error("Errore nel recupero dei prodotti");
+
+
+    return response.json()
+}
+
 export async function getAllProducts(): Promise<Product[]>{
     const response= await fetch(`${BASE_URL}/products`);
 
